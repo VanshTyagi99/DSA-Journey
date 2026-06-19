19ddbdba78030105c7130f0c646dd96730f32ef3
Vansh Tyagi <vanshtyagi7032@gmail.com>
2026-06-19 12:13:32 +0530

    Addesd Rotated Array Solution

---
+++ ARRAY/rotatedarray.java
@@ -0,0 +1,46 @@
+class rotatedarray{
+    public static void main(String[] args) {
+        int [] arr={3,4,6,8,9,12,4};
+        int n=arr.length;
+        int i=0;
+        int d=3;   //rotated array position-means 3rd se array ko rotate karna hai 
+        int m=d-1;
+        int j=n-1;        
+
+
+    System.out.println("  ");
+
+        while(i<m){
+           int temp=arr[i];
+           arr[i]=arr[m];
+           arr[m]=temp;
+           i++;
+           m--;
+        }
+       
+   
+
+         while(d<j){
+           int temp=arr[d];
+           arr[d]=arr[j];
+           arr[j]=temp;
+           d++;
+           j--;
+        }
+i = 0;
+j = n - 1;
+
+     System.out.println("  ");
+          while(i<j){
+           int temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+           i++;
+           j--;
+        }
+        
+    for(int ele: arr){
+        System.out.print(" "+ele);
+    }
+    }
+}
