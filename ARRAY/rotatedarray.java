7cb19494e6254a77ddfff0ac0a545304123470aa
Vansh Tyagi <vanshtyagi7032@gmail.com>
2026-06-20 11:11:49 +0530

    Updated rotated array code

--- ARRAY/rotatedarray.java
+++ ARRAY/rotatedarray.java
@@ -2,12 +2,16 @@ class rotatedarray{
     public static void main(String[] args) {
         int [] arr={3,4,6,8,9,12,4};
         int n=arr.length;
-        int i=0;
+     //   int i=0;
         int d=3;   //rotated array position-means 3rd se array ko rotate karna hai 
-        int m=d-1;
-        int j=n-1;        
+    //    int m=d-1;
+    //    int j=n-1;      
+    
+    reverse(arr,0,d-1);
+    reverse(arr,d,n-1);
+    reverse(arr,0,n-1);
 
-
+/* 
     System.out.println("  ");
 
         while(i<m){
@@ -38,9 +42,28 @@ j = n - 1;
            i++;
            j--;
         }
-        
+     */   
     for(int ele: arr){
         System.out.print(" "+ele);
     }
+
+    
+
+
+
     }
+
+   
+
+    static void reverse(int arr[],int i, int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        
+    }
+
 }
